ed3f34789f04cc9c25af8da40ae9b58b39a8474f
thommythompson <38915689+thommythompson@users.noreply.github.com>
2018-05-09 13:15:32 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -11,5 +11,3 @@ New-Item -ItemType File -Path "C:\cc4skype_info.txt" -Force
 $DC | Out-File "C:\cc4skype_info.txt" -Append
 
 $SFB | Out-File "C:\cc4skype_info.txt" -Append
-
-powerShell.exe -NonInteractive -NoProfile -ExecutionPolicy Bypass ./install-cc4skype.ps1 -DC $DC -SFB $SFB
